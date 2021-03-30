00b2f179d253ede9ce68d5f3e12e512bd7afc3a5
AdiWeit <adi004@web.de>
2021-03-30 10:34:14 +0200

    points and possibilities to spend them

--- Tetris-JavaScript-master/tetris.js
+++ Tetris-JavaScript-master/tetris.js
@@ -20,6 +20,7 @@ function drawSquare(x,y,color, board){
 // create the board
 
 let board = [[], []];
+let shop = [];
 for (var i = 0; i < board.length; i++) {
 for( r = 0; r <ROW; r++){
     board[i][r] = [];
@@ -33,6 +34,7 @@ const ctx = [];
 for (var i = 0; i < board.length; i++) {
   ctx[i] = document.getElementById("tetris " + i).getContext("2d");
 }
+ctx[i] = document.getElementById("shop").getContext("2d");
 const scoreElement = document.getElementById("score");
 
 // draw the board
@@ -60,12 +62,33 @@ const PIECES = [
     [I,"cyan"],
     [J,"orange"]
 ];
-
+const addTiles = [
+  [one, "red"],
+  [two, "green"],
+  [edge, "blue"]
+]
+for (var i = 0; i < 20; i++) {
+  shop[i] = [];
+  for (var i1 = 0; i1 < 3; i1++) {
+    shop[i][i1] = VACANT;
+  }
+}
+var shopTiles = {forcedTiles: [], fillTiles: []};
+for (var i = 0; i < PIECES.length; i++) {
+  shopTiles.forcedTiles.push(new Piece( PIECES[i][0],PIECES[i][1], board.length, i*5, 0));
+}
+for (var i = 0; i < addTiles.length; i++) {
+  shopTiles.fillTiles.push(new Piece( addTiles[i][0],addTiles[i][1], board.length, i*5, 4));
+}
 // generate random pieces
 
 function randomPiece(board){
   // for (var i = 0; i < board.length; i++) {
     let r = randomN = Math.floor(Math.random() * PIECES.length) // 0 -> 6
+    if (nextStone && nextStone[board] != undefined) {
+      r = nextStone[board];
+      nextStone[board] = undefined;
+    }
     return new Piece( PIECES[r][0],PIECES[r][1], board);
   // }
 }
@@ -77,7 +100,7 @@ for (var i = 0; i < board.length; i++) {
 
 // The Object Piece
 
-function Piece(tetromino,color, board){
+function Piece(tetromino,color, board, x, y){
     this.tetromino = tetromino;
     this.color = color;
 
@@ -86,7 +109,9 @@ function Piece(tetromino,color, board){
 
     // we need to control the pieces
     this.x = 3;
+    if (x != undefined) this.x = x;
     this.y = -2;
+    if (y != undefined) this.y = y;
     this.board = board;
     this.locked = false;
     this.waitingForNewPiece = false;
@@ -194,6 +219,7 @@ Piece.prototype.rotate = function(){
 }
 
 let score = 0;
+var points = 0;
 
 Piece.prototype.lock = function(){
   console.log("lock");
@@ -236,11 +262,13 @@ Piece.prototype.lock = function(){
             }
             // increment the score
             score += 10;
+            points += 5;
         }
     }
 
     // update the score
     scoreElement.innerHTML = score;
+    document.getElementById("points").innerHTML = points;
 }
 
 // collision fucntion
@@ -278,25 +306,40 @@ Piece.prototype.collision = function(x,y,piece){
     return false;
 }
 
+for (var i = 0; i < PIECES.length; i++) {
+  shopTiles.forcedTiles[i].draw();
+}
+for (var i = 0; i < addTiles.length; i++) {
+  shopTiles.fillTiles[i].draw();
+}
+
 // CONTROL the piece
 
 document.addEventListener("keydown",CONTROL);
-
+var pause = false;
 function CONTROL(event){
   for (var i = 0; i < board.length; i++) {
-    if(event.keyCode == 37){
+    if(event.keyCode == 37 && !pause){
         p[p.length - 1 - i].moveLeft();
         dropStart = Date.now();
     }else if(event.keyCode == 38){
+      if (!fillStone) {
         p[p.length - 1 - i].rotate();
         dropStart = Date.now();
-    }else if(event.keyCode == 39){
+      }
+      else if (i == board.length - 1) {
+        fillStone.rotate();
+        dropStart = Date.now();
+      }
+    }else if(event.keyCode == 39 && !pause){
         p[p.length - 1 - i].moveRight();
         dropStart = Date.now();
-    }else if(event.keyCode == 40){
+    }else if(event.keyCode == 40 && !pause){
         p[p.length - 1 - i].moveDown();
     }
   }
+  if (event.key == "p") pause = !pause
+  if (!pause && !gameOver) drop();
 }
 
 // drop the piece every 1sec
@@ -305,7 +348,7 @@ let dropStart = Date.now();
 let gameOver = false;
 var addedI = 0;
 function drop(){
-  console.log("drop");
+  // console.log("drop");
     let now = Date.now();
     let delta = now - dropStart;
     if(delta > 1000){
@@ -319,9 +362,99 @@ function drop(){
       addedI = 0;
         dropStart = Date.now();
     }
-    if( !gameOver){
+    if( !gameOver && !pause){
         requestAnimationFrame(drop);
     }
 }
 
 drop();
+document.onmousemove = readMouseMove
+var xMaus;
+var yMaus;
+var storedPosition = {};
+function readMouseMove(e) {
+  xMaus = e.clientX //- 10;
+  yMaus = e.clientY //+ 23//- 33;
+  if (fillStone) {
+    var boardX = xMaus - document.getElementById("tetris " + fillStone.board).getBoundingClientRect().x// - scrollX;
+    var boardY = yMaus - document.getElementById("tetris " + fillStone.board).getBoundingClientRect().y //- scrollY;
+    if (collisionFilling(fillStone.x, fillStone.y) && !collisionFilling(JSON.parse((boardX/SQ + "").split(".")[0]), JSON.parse((boardY/SQ + "").split(".")[0]))) {
+      fillStone.x = storedPosition.x;
+      fillStone.y = storedPosition.y;
+    }
+    if (!collisionFilling(fillStone.x, fillStone.y) && !collisionFilling(JSON.parse((boardX/SQ + "").split(".")[0]), JSON.parse((boardY/SQ + "").split(".")[0]))) fillStone.unDraw();
+    if (collisionFilling(JSON.parse((boardX/SQ + "").split(".")[0]), JSON.parse((boardY/SQ + "").split(".")[0])) && !collisionFilling(fillStone.x, fillStone.y)) {
+      storedPosition = {x: fillStone.x, y: fillStone.y};
+    }
+    fillStone.x = JSON.parse((boardX/SQ + "").split(".")[0]);
+    fillStone.y = JSON.parse((boardY/SQ + "").split(".")[0]);
+    if (!collisionFilling(fillStone.x, fillStone.y)) {
+    fillStone.draw();
+  }
+  }
+}
+function collisionFilling(x, y) {
+  var fallingFields = {x: [], y: []};
+  for (var i = 0; i < p[p.length - 2 + fillStone.board].activeTetromino.length; i++) {
+    for (var i1 = 0; i1 < p[p.length - 2 + fillStone.board].activeTetromino[i].length; i1++) {
+      if (p[p.length - 2 + fillStone.board].activeTetromino[i][i1] == 1) {
+        fallingFields.y.push(p[p.length - 2 + fillStone.board].y + i);
+        fallingFields.x.push(p[p.length - 2 + fillStone.board].x + i1);
+      }
+    }
+  }
+  for (var i = 0; i < fillStone.activeTetromino.length; i++) {
+    for (var i1 = 0; i1 < fillStone.activeTetromino[i].length; i1++) {
+        if (fillStone.activeTetromino[i][i1] == 1 && x >= 0 && y >= 0 && y < board[fillStone.board].length && x < board[fillStone.board][y].length && (board[fillStone.board][y + i][x + i1] != VACANT)) {
+          return true;
+        }
+        for (var i2 = 0; i2 < fallingFields.x.length; i2++) {
+          if (fallingFields.x[i2] == x + i1 && fallingFields.y[i2] == y + i && fillStone.activeTetromino[i][i1] == 1) return true;
+        }
+    }
+  }
+  if (!(x >= 0 && y >= 0 && y < board[fillStone.board].length && x < board[fillStone.board][y].length)) {
+    return true;
+  }
+  return false;
+}
+function bodyClicked() {
+  if (fillStone && !collisionFilling(fillStone.x, fillStone.y) && confirm('Wollen sie hier wirklich den Stein festsetzen? Wenn ja, drücken sie danach "p", um das Spiel fortzuführen!')) {
+    fillStone.lock();
+    // TODO: Ränder füllen
+    // TODO: Spielbrett auch bei pause neu zeichnen bzw. Pause beim locken aufheben
+    fillStone = undefined;
+  }
+}
+var fillStone;
+var requiredPoints = {forcedTiles: [3, 3, 4, 4, 3, 5, 3], fillTiles: [8, 7, 4]};
+var nextStone = [];
+function shopClicked() {
+  var x = xMaus - document.getElementById("shop").getBoundingClientRect().x;
+  var y = yMaus - document.getElementById("shop").getBoundingClientRect().y;
+  for (var i = 0; i < PIECES.length; i++) {
+    if (x > SQ*i*5 && x < SQ*(i + 1)*5 && y > 0 && y < SQ*3){
+      if (points >= requiredPoints.forcedTiles[i]) {
+      var board = prompt('Für das wievielte Spielbrett wollen sie den nächsten Stein (als ' + PIECES[i][1] + ') festlegen? Sie verlieren dabei ' + requiredPoints.forcedTiles[i] + ' Punkte! Wenn sie sich verklickt haben, klicken sie bitte auf "abbrechen"');
+      if (board && !isNaN(board)) {
+          points -= requiredPoints.forcedTiles[i];
+          document.getElementById("points").innerHTML = points;
+          nextStone[board - 1] = i;
+        }
+      }
+      else alert("Sie haben zu wenig Punkte, um diesen Stein reservieren zu können!");
+    }
+    if (x > SQ*i*5 && x < SQ*(i + 1)*5 && y > SQ*5 && y < SQ*7){
+      if (points >= requiredPoints.fillTiles[i]) {
+      var board = prompt('In das wievielte Spielbrett wollen sie den Stein einsetzen?');
+      if (confirm("Sind sie sich sicher, dass sie " + requiredPoints.fillTiles[i] + " Punkte für den ausgewählten Stein (" + addTiles[i][1] + ") zum Einsetzen bezahlen wollen?")) {
+        pause = true;
+        points -= requiredPoints.fillTiles[i];
+        document.getElementById("points").innerHTML = points;
+        fillStone = new Piece( addTiles[i][0], addTiles[i][1], board - 1, -4, -4)
+    }
+    }
+    else alert("Sie brauchen " + requiredPoints.fillTiles[i] + " Punkte, um sich diesen Stein zum Einsetzen leisten zu können!");
+  }
+  }
+}
